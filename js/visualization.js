1bc6cd5e26f6935a28d5239629423964492746d2
yuanshuo-wu <yuanshuowu200502@gmail.com>
2024-12-16 18:12:46 -0500

    upload Demo video

--- js/visualization.js
+++ js/visualization.js
@@ -1,12 +1,12 @@
-// Immediately Invoked Function Expression to limit access to our 
-// variables and prevent 
+// Immediately Invoked Function Expression to limit access to our
+// variables and prevent
 ((() => {
 
-  console.log("Hello, world!");
+  //console.log("Hello, world!");
   d3.json("data/Scatterplot_data.json", (data) => {
     data.forEach(d => {
       d.secs = +d.secs;
-      d.mem = +d.mem;    
+      d.mem = +d.mem;
       d.Average_cpu_load = parseFloat(d.Average_cpu_load.replace("%", ""));
     })
     const dispatchString = "selectionUpdated";
@@ -19,11 +19,11 @@
       .yLabelOffset(50)
       .selectionDispatcher(d3.dispatch(dispatchString))
       ("#scatterplot", data);
-    
+
     let tableChart = table_scatterplot()
       .selectionDispatcher(d3.dispatch(dispatchString))
       ("#table-scatterplot-container", data);
-    
+
     scatterplotChart.selectionDispatcher().on(dispatchString, function(selectedData) {
         tableChart.updateSelection(selectedData);
     });
